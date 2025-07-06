bbaedb44d044206f68d30ebafadf5b3a65bfd697
WojciechMycek <wojciech.mycek97@gmail.com>
2025-07-06 21:58:14 +0200

    restore home

--- home_catolog_backup.sh
+++ home_catolog_backup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 echo "Proceeding with homefolder backup"
-cp -r /home /back_home
+cp -r home/ back_home/
 echo "Done"
+
+#delete main home folder
+
+rm -r home/
+
+#restore home folder from backup
+
+cp -r back_home/home .
+
+echo "Home folder is restored"
