6584f11a17c4ed038e81fc5bb7938a797edb071a
WojciechMycek <wojciech.mycek97@gmail.com>
2025-07-09 19:15:28 +0200

    Check if netstat is installed

--- 4_check_which_ports_are_open.sh
+++ 4_check_which_ports_are_open.sh
@@ -3,3 +3,12 @@
 defined_port_by_user=$1
 echo "Check. $defined_port_by_user"
 
+if ! command -v netstat &> /dev/null; then
+    echo "netstat not found. Installing..."
+    sudo apt update
+    sudo apt install -y net-tools
+else
+    echo "netstat is already installed."
+    netstat | grep "$1"
+fi
+
